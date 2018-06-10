1028bca7e706c483edded638ab3165a498e761a7
MikeyPdog <mikeypdog911@gmail.com>
2018-06-10 18:39:54 +0100

    First minimod - faster searching

---
+++ PatchFasterSearch.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+
+namespace FasterSearching
+{
+    [HarmonyPatch(typeof(Container))]
+    [HarmonyPatch("BeginContainerInteraction")]
+    class PatchFasterSearch
+    {
+        static void Prefix(ref float searchTimeSeconds, ref Container __instance)
+        {
+            if (__instance.m_Items.Count == 0)
+            {
+                searchTimeSeconds = 0f;
+            }
+            else
+            {
+                searchTimeSeconds *= 0.3f;
+            }
+        }
+    }
+}
