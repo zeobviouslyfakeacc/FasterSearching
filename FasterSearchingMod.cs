6da82f37a6aa246f1c81ab965e4ff66b7405bea5
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2020-07-20 20:44:23 +0200

    Port to MelonLoader

---
+++ FasterSearchingMod.cs
@@ -0,0 +1,11 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace FasterSearching {
+
+	internal class FasterSearchingMod : MelonMod {
+		public override void OnApplicationStart() {
+			Debug.Log($"[{InfoAttribute.Name}] version {InfoAttribute.Version} loaded!");
+		}
+	}
+}
--- PatchFasterSearch.cs
+++ PatchFasterSearch.cs
@@ -1,21 +1,17 @@
 ﻿using Harmony;
 
-namespace FasterSearching
-{
-    [HarmonyPatch(typeof(Container))]
-    [HarmonyPatch("BeginContainerInteraction")]
-    class PatchFasterSearch
-    {
-        static void Prefix(ref float searchTimeSeconds, ref Container __instance)
-        {
-            if (__instance.m_Items.Count == 0)
-            {
-                searchTimeSeconds = 0f;
-            }
-            else
-            {
-                searchTimeSeconds *= 0.3f;
-            }
-        }
-    }
+namespace FasterSearching {
+
+	[HarmonyPatch(typeof(Container))]
+	[HarmonyPatch("BeginContainerInteraction")]
+	internal static class PatchFasterSearch {
+
+		static void Prefix(ref float searchTimeSeconds, ref Container __instance) {
+			if (__instance.m_Items.Count == 0) {
+				searchTimeSeconds = 0f;
+			} else {
+				searchTimeSeconds *= 0.3f;
+			}
+		}
+	}
 }
