a539a0bde04f58cdad9d1122627f6cbbd766d244
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2023-01-10 23:58:36 +0100

    Style clean-up

--- FasterSearchingMod.cs
+++ FasterSearchingMod.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace FasterSearching {
 
 	internal class FasterSearchingMod : MelonMod {
-        public override void OnInitializeMelon() {
+		public override void OnInitializeMelon() {
 			FasterSearchingSettings.Instance = new FasterSearchingSettings();
 			FasterSearchingSettings.Instance.AddToModSettings("Faster Searching");
 			Debug.Log($"[{Info.Name}] version {Info.Version} loaded!");
--- PatchFasterSearch.cs
+++ PatchFasterSearch.cs
@@ -3,16 +3,14 @@ using Il2Cpp;
 
 namespace FasterSearching {
 
-    [HarmonyPatch(typeof(ContainerInteraction), "PerformHold")]
-    internal static class PatchFasterSearchUpdated
-    {
-        static void Postfix(ref ContainerInteraction __instance)
-        {
-            if (__instance.m_Container.m_Items.Count == 0 && FasterSearchingSettings.Instance.instantlySearchEmpty)
-            {
-                __instance.HoldTime = 0;
-            }
-            else __instance.HoldTime *= FasterSearchingSettings.Instance.searchTimeMultiplier;
-        }
-    }
+	[HarmonyPatch(typeof(ContainerInteraction), "PerformHold")]
+	internal static class PatchFasterSearchUpdated {
+		static void Postfix(ContainerInteraction __instance) {
+			if (__instance.m_Container.m_Items.Count == 0 && FasterSearchingSettings.Instance.instantlySearchEmpty) {
+				__instance.HoldTime = 0;
+			} else {
+				__instance.HoldTime *= FasterSearchingSettings.Instance.searchTimeMultiplier;
+			}
+		}
+	}
 }
