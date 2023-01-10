b1be96ffe3e5d3140934fcb4a98ccf3048550c06
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2023-01-11 00:56:57 +0100

    Fix patch not working as intended

--- PatchFasterSearch.cs
+++ PatchFasterSearch.cs
@@ -3,13 +3,16 @@ using Il2Cpp;
 
 namespace FasterSearching {
 
-	[HarmonyPatch(typeof(ContainerInteraction), "PerformHold")]
+	[HarmonyPatch(typeof(Container), "BeginContainerOpen")]
 	internal static class PatchFasterSearchUpdated {
-		static void Postfix(ContainerInteraction __instance) {
-			if (__instance.m_Container.m_Items.Count == 0 && FasterSearchingSettings.Instance.instantlySearchEmpty) {
-				__instance.HoldTime = 0;
+		static void Prefix(Container __instance) {
+			ContainerInteraction interaction = __instance.GetComponent<ContainerInteraction>();
+			if (interaction == null) return;
+
+			if (__instance.m_GearToInstantiate.Count == 0 && !__instance.IsInspected() && FasterSearchingSettings.Instance.instantlySearchEmpty) {
+				interaction.HoldTime = 0;
 			} else {
-				__instance.HoldTime *= FasterSearchingSettings.Instance.searchTimeMultiplier;
+				interaction.HoldTime *= FasterSearchingSettings.Instance.searchTimeMultiplier;
 			}
 		}
 	}
